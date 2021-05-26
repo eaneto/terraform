e075c55196f7e24856348dda1787043cfd89f454
Edison Neto <edison.aguiar.neto@gmail.com>
2021-05-26 20:58:26 -0300

    Add dynamodb and sqs example using localstack

---
+++ README.md
@@ -0,0 +1,9 @@
+# Terraform
+
+Just writing some terraform that i have used in other projects.
+
+```bash
+docker-compose up -d
+terraform init
+terraform apply -auto-approve
+```
--- main.tf
+++ main.tf
@@ -9,15 +9,18 @@ terraform {
 
 # Configure the AWS Provider
 provider "aws" {
-  region = "us-east-1"
-}
+  access_key                  = "mock_access_key"
+  region                      = "us-east-1"
+  s3_force_path_style         = true
+  secret_key                  = "mock_secret_key"
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
 
-# Create a VPC
-resource "aws_vpc" "example" {
-  cidr_block = "10.0.0.0/16"
-
-  tags = {
-    Name = "example"
+  endpoints {
+    dynamodb = "http://localhost:4566"
+    sqs      = "http://localhost:4566"
+    s3       = "http://localhost:4566"
   }
 }
 
@@ -29,5 +32,52 @@ resource "aws_s3_bucket" "examplebucket" {
   tags = {
     Name        = "My bucket"
     Environment = "Dev"
+  }
+}
+
+# Create DynamoDB Table
+resource "aws_dynamodb_table" "basic-dynamodb-table" {
+  name           = "my-table"
+  billing_mode   = "PROVISIONED"
+  read_capacity  = 1
+  write_capacity = 1
+  hash_key       = "some-id"
+
+  attribute {
+    name = "some-id"
+    type = "S"
+  }
+
+  tags = {
+    Environment = "dev"
+  }
+}
+
+# Create SQS Queue
+resource "aws_sqs_queue" "example_queue" {
+  name                      = "terraform-example-queue"
+  delay_seconds             = 90 # 3 seconds
+  max_message_size          = 2048 # 2K
+  message_retention_seconds = 86400 # 1 day
+  receive_wait_time_seconds = 10
+  # Retryfour times
+  redrive_policy = jsonencode({
+    deadLetterTargetArn = aws_sqs_queue.example_queue_dlq.arn
+    maxReceiveCount     = 4
+  })
+
+  tags = {
+    Environment = "dev"
+  }
+}
+
+# Create SQS DLQ
+resource "aws_sqs_queue" "example_queue_dlq" {
+  name                      = "terraform-example-queue-dlq"
+  max_message_size          = 2048 # 2K
+  message_retention_seconds = 1209600 # 14 days
+
+  tags = {
+    Environment = "dev"
   }
 }
